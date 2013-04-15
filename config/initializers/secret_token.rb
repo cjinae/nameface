ebdb623991fbc4d838fc42ac3c98a5fc6701373b
Christine Lee <christine.jinae@gmail.com>
2013-04-15 11:24:46 -0400

    initila commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nameface::Application.config.secret_token = '2cd18dd5ad737af223ded66912604942e532a5b8b715d27662cc0d82311d1c42f74b6b1a4037f6f601f19064adcd663cf914c44bc50803594391c332752ed962'
