8316b21c46b34675371b3274cd5c9758d2eb11ae
Christine Lee <christine.jinae@gmail.com>
2013-04-16 15:50:48 -0400

    fix onclick of rightname call flip actoin

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -10,66 +10,82 @@ $.ajax({
 	success: function(data){
 		console.log(data);
 	}
- })
+ });
 
-$('.thumbnail img').click(function(){
-	console.log('clicked');
-	// $(this).children('h4').slideToggle(300);
+$('.thumbnail .imgcontainer').click(function(){
+	// console.log('clicked');
 	$(this).siblings('h4').slideToggle(300);
 })
 
+$('.wrongname').click(function(e) {
+  console.log($(this).text());
+  alert("Try Again!");
+  $(this).remove();
+});
+
 $('.realname').click(function(e) {
 	console.log($(this).text());
-	alert("Correct!");
 	e.stopPropagation();  //stops action from going one level up
-})
+	flip(this);
+	// alert("Correct!");
+});
 
-$('.wrongname').click(function(e) {
-	console.log($(this).text()); 
-	alert("Try Again!");
-	$(this).remove();
-})
-
-
-//flip
-
+//flip action
 
 var margin =$(".image1").width()/2;
 var width=$(".image1").width();
 var height=$(".image1").height();
-$(".image2").stop().css({width:'0px',height:''+height+'px',marginLeft:''+margin+'px',opacity:'0.5'});
-// $("#reflection2").stop().css({width:'0px',height:''+height+'px',marginLeft:''+margin+'px'});
-	$(".image1").click(function(){
+
+function flip(image) {
+	$(".image2").stop().css({
+		width:'0px',
+		height:''+height+'px',
+		marginLeft:''+margin+'px',
+		opacity:'0.5'
+	});
+	$(image).siblings('.imgcontainer').find('.image1').animate({
+		width:'0px',
+		height:''+height+'px',
+		marginLeft:''+margin+'px',
+		opacity:'0.5'
+	}, 500, function (){
+		//animate complete
+		// '<div>Animation complete.</div>'
+		
+		this.src = '/assets/nameis.png';
 		$(this).stop().animate({
-			width:'0px',
+			width:''+width+'px',
 			height:''+height+'px',
-			marginLeft:''+margin+'px',
-			opacity:'0.5'
-		}, 500, function (){
-			//animate complete
-			// '<div>Animation complete.</div>'
-			this.src = '/assets/nameis.png';
-			$(this).stop().animate({
-				width:''+width+'px',
-				height:''+height+'px',
-				marginLeft:'0',
-				opacity:'1'
-			}, 500, function() {
-				// unbind the click event from this
-				$(this).unbind('click')
-			});
+			marginLeft:'0',
+			opacity:'1'
+		}, 500, function() {
+			// unbind the click event from this
+			// $(this).unbind('click')
 		});
 	});
-	// 	window.setTimeout(function() {
-	// 	$(".image2").stop().animate({width:''+width+'px',height:''+height+'px',marginLeft:'0px',opacity:'1'},{duration:500});
-	// 	},500);
-	// });
-	// $(".image2").click(function(){
-	// 	$(this).stop().animate({width:'0px',height:''+height+'px',marginLeft:''+margin+'px',opacity:'0.5'},{duration:500});
-	// 	window.setTimeout(function() {
-	// 	$(".image1").stop().animate({width:''+width+'px',height:''+height+'px',marginLeft:'0px',opacity:'1'},{duration:500});
-	// 	},500);
-	// });
+}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//end
 });
 
