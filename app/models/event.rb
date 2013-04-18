15045a9a5a2098b1d08d73e303019af901e4d21b
Christine Lee <christine.jinae@gmail.com>
2013-04-17 22:01:41 -0400

    add validation to event create. Style to start now

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,12 @@
 class Event < ActiveRecord::Base
-	attr_accessible :title
+	attr_accessible :title, :description, :avatar
+    
+    has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"  
+	validates :title, :presence => true, :uniqueness => true
+  	validates :description, :presence => true
 
 	has_many :assignments
 	has_many :users, through: :assignments
-
-
-
-
 
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
    attr_accessible :email, :password, :password_confirmation, :first_name, :last_name, :cardview, :avatar
-   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"  
-
- has_many :assignments
- has_many :events, through: :assignments
-   
+  
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"  
   validates :first_name, :presence => true
   validates :last_name, :presence => true
   validates :email, :presence => true, :uniqueness => true
   validates :password, :presence => true, :on => :create
 
+  has_many :assignments
+  has_many :events, through: :assignments
+   
   # validates_attachment :avatar, :presence => true,
   # :content_type => { :content_type => "image/jpg" },
   # :size => { :in => 0..10.kilobytes }
