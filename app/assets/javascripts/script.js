fe0844c9b8bf069322650811de18853625a85cd2
Christine Lee <christine.jinae@gmail.com>
2013-04-24 21:29:24 -0400

    change hover state of faces and fix grayscale

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -12,6 +12,8 @@ $(document).ready(function(){
 // 	}
 //  });
 
+
+// animate on hover of face
 $(function(){
 	$('img.faceis').each(function(){
 		var x = $(this);
@@ -19,39 +21,39 @@ $(function(){
 		var target = x.css('background-image')
 			// remove the right and left braces, g to continue finiding and replace with ''
 			.replace(/^url|[\)\()]/g, '');
-			// console.log(target);
+			console.log(target);
 
-			x.wrap('<span style = "position: relative;"></span>')
-				.parent() //span elements
-				.prepend('<img>')
-				.find('img:first') // selector is now new blank image
-				.attr('src', target)
+		x.wrap('<span style = "position: relative;"></span>')
+			.parent() //span elements
+			.prepend('<img class = "overlay">')
+			.find('img:first') // selector is now new blank image
+			.attr('src', target)
 
-			x.css({
-				'position' : 'absolute',
-				'left' : 0,
-				// 'top' : this.offsetTop
-			});
-
-			x.hover(function(){
-				x.stop().animate({
-					opacity: 0.4 
-				}, 250);
-			}, function(){
-				x.stop().animate({
-				 	opacity:1	
-				}, 3000);
-			});
+		x.css({
+			'position' : 'absolute',
+			'left' : 0,
+			// 'top' : this.offsetTop
 		});
+
+		x.hover(function(){
+			x.stop().animate({
+				opacity: 0.4 
+			}, 250);
+		}, function(){
+			x.stop().animate({
+			 	opacity:1	
+			}, 250);
+		});
+	});
 });
 
-
+// remove guess background on click of names
+// animate on click of names
 $('.thumbnail .imgcontainer').click(function(){
 	console.log(this)
-	var x = $(this);
-	x.siblings('.namespacer').find('.namelist').slideToggle(300);
-})
-
+	$(this).siblings('.namespacer').find('.namelist').slideToggle(300);
+	$(this).find('.overlay').css('visibility','hidden');
+});
 
 $('.wrongname').click(function(e) {
   console.log($(this).text());
@@ -61,9 +63,14 @@ $('.wrongname').click(function(e) {
 $('.realname').click(function(e) {
 	console.log($(this).text());
 	e.stopPropagation();  //stops action from going one level up
-	$(this).css('color','#53777a');
-	$(this).siblings('.wrongname').remove();
+	var x = $(this)
+	x.css('color','#53777a');
+	x.siblings('.wrongname').remove();
+	// var y = x.parents('.namespacer').siblings('.imgcontainer').find('.faceis');
+	// y.removeClass('desaturate');
+	// console.log(y)
 	flip(this);
+	
 	// alert("Correct!");
 });
 
@@ -88,13 +95,14 @@ function flip(name) {
 		//animate complete
 		// '<div>Animation complete.</div>'
 		
-		this.src = '/assets/woohoo.jpg';
+		// this.src = '/assets/owl.png';
 		$(this).stop().animate({
 			width:''+width+'px',
 			height:''+height+'px',
 			marginLeft:'0',
 			opacity:'1'
 		}, 500, function() {
+			$(this).removeClass('desaturate')
 			// unbind the click event from this
 			// $(this).unbind('click')
 		});
