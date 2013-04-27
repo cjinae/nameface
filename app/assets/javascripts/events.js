5a7d60ae55bd459d4ae0075afde889ae0c05986c
Christine Lee <christine.jinae@gmail.com>
2013-04-26 22:15:13 -0400

    change style of add and remove btn on event page

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -2,3 +2,14 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
+$(document).ready(function(){
+	
+$('.action').hover(function(){
+	$(this).css('opacity', '1');
+	// $(this).css('opacity','0.4');
+});
+
+
+
+
+})
