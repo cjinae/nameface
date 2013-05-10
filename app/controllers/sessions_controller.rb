72f0d190f5459140858d79cfcc721b85ff4be03d
Christine Lee <christine.jinae@gmail.com>
2013-05-10 13:30:11 -0400

    test feature branch2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,22 +2,22 @@ class SessionsController < ApplicationController
   # def new 
   # end
 
-  def create
-    @user = login(params[:email], params[:password])
-    if @user
-      redirect_to user_path(@user), :notice => "Welcome back"
-    else
-      flash.now[:alert] = "Invalid credentials. Try again?"
-      render :new
-    end
-  end
+  # def create
+  #   @user = login(params[:email], params[:password])
+  #   if @user
+  #     redirect_to user_path(@user), :notice => "Welcome back"
+  #   else
+  #     flash.now[:alert] = "Invalid credentials. Try again?"
+  #     render :new
+  #   end
+  # end
 
 # For the Linked-In API VERSION
-  # def create
-  #   user = User.from_omniauth(env['omniauth.auth'])
-  #   session[:user_id] = user.id
-  #   redirect_to user_path(current_user), notice: "Signed in."
-  # end
+  def create
+    user = User.from_omniauth(env['omniauth.auth'])
+    session[:user_id] = user.id
+    redirect_to user_path(current_user), notice: "Signed in."
+  end
 
   def destroy
     logout
